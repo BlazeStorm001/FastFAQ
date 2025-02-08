5052d6275225749fa65ddabc4a56820b3fb81d95
Namit Arora <namit0010@gmail.com>
2025-02-08 19:42:26 +0530

    fix: Added middleware to main.py

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from app.database.session import engine, Base
 from fastapi import FastAPI
 from app.api import faqs
-
+from app.middleware import add_cors_middleware
 
 # Initialize FastAPI app
 app = FastAPI()
@@ -9,7 +9,7 @@ app = FastAPI()
 # Create tables
 Base.metadata.create_all(bind=engine)
 
-
+add_cors_middleware(app)
 
 app.include_router(faqs.router, prefix="/api", tags=["FAQs"])
 
